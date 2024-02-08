c7f19b7988386cc4042024ac09126ac968549520
Vijay Singh Rathore <rathorevijay995@gmail.com>
2024-02-08 09:16:38 +0530

    Time: 38 ms (99.02%) | Memory: 47.5 MB (37.64%) - LeetSync

---
+++ 812-rotate-string/rotate-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+   let newStr = s+s+s;
+   if(newStr.includes(goal) && s.length===goal.length) return true;
+   return false; 
+};
