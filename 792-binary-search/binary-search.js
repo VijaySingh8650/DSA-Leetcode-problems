6d6b46b057e3600ab9745d9bae7aa8747ecb8a9c
Vijay Singh Rathore <rathorevijay995@gmail.com>
2024-02-08 07:14:15 +0530

    Time: 46 ms (94.82%) | Memory: 51.3 MB (31.36%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -20,7 +20,7 @@ var search = function(nums, target) {
         let mid = Math.floor((left+right)/2);
         if(nums[mid]===target) return mid;
         else if(nums[mid]>target)right=mid-1;
-        else left = mid+1;
+        else if(nums[mid]<target)left = mid+1;
     }
     return -1;
     
