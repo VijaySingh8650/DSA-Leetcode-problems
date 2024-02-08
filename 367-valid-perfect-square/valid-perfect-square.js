c2eaf987726513d0a5dd44d433791198563ce574
Vijay Singh Rathore <rathorevijay995@gmail.com>
2024-02-08 08:15:20 +0530

    Time: 48 ms (79.55%) | Memory: 48.9 MB (12.89%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.js
+++ 367-valid-perfect-square/valid-perfect-square.js
@@ -3,10 +3,23 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-
-    for(let i=1; i<=num;i++){
-          if(i*i===num)return true;
-          else if(i*i > num) return false;
-    }
     
+    // TC-O(sqrt(n))
+    // for(let i=1; i<=num;i++){
+    //       if(i*i===num)return true;
+    //       else if(i*i > num) return false;
+    // }
+    
+    //TC- O(log(n))
+    let left = 0;
+    let right = num;
+    while(left<=right){
+
+        let mid = Math.floor((left+right)/2);
+        if(mid*mid === num)return true;
+        else if(mid*mid > num) right = mid-1;
+        else if(mid*mid < num)  left = mid+1;
+
+    }
+    return false;
 };
