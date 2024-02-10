dfbed97813e5a78e3c4668b713e4ed003c0be2e1
Vijay Singh Rathore <rathorevijay995@gmail.com>
2024-02-10 08:52:55 +0530

    Time: 51 ms (66.34%) | Memory: 48.7 MB (30.08%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -5,13 +5,31 @@
 var findPeakElement = function(nums) {
     if(nums.length === 1) return 0;
 
-    for(let i=0; i<nums.length; i++){
+    // for(let i=0; i<nums.length; i++){
 
-        if(i===0 && nums[i] > nums[i+1]) return i;
+    //     if(i===0 && nums[i] > nums[i+1]) return i;
         
-        if(i===nums.length-1 && nums[i]>nums[i-1]) return i;
+    //     if(i===nums.length-1 && nums[i]>nums[i-1]) return i;
         
-        if(nums[i-1]<nums[i] && nums[i]> nums[i+1]) return i;
-        
+    //     if(nums[i-1]<nums[i] && nums[i]> nums[i+1]) return i;
+
+    // }
+    
+    let left = 0;
+    let right = nums.length-1;
+
+    while(left<right){
+
+        let mid = left + Math.floor((right-left)/2);
+        if(mid>0 && nums[mid] < nums[mid-1]) right = mid-1;
+        else if(mid<nums.length-1 && nums[mid]<nums[mid+1]){
+                  left = mid+1;
+        }
+        else{
+            return mid;
+        }
     }
+
+    return left;
+
 };
