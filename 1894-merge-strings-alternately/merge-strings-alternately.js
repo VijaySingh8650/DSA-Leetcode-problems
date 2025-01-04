1817d1126f518d4fac04bc71dc1c184d1c5b8083
Vijay Singh Rathore <rathorevijay995@gmail.com>
2025-01-04 13:59:00 +0530

    Time: 62 ms (15.32%) | Memory: 50.7 MB (11.95%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -6,18 +6,47 @@
 var mergeAlternately = function(word1, word2) {
 
     let result = "";
+    
     // brute force
-    for(let i=0; i< (word1.length + word2.length); i++){
+    // for(let i=0; i< (word1.length + word2.length); i++){ // TC - O(n+m) SC - (1)
 
-        if(word1[i]){
-            result+= word1[i];
+    //     if(word1[i]){
+    //         result+= word1[i];
+    //     }
+    //     if(word2[i]){
+    //        result+= word2[i];
+    //     }
+
+    // }
+
+    // return result;
+   
+
+   //Optimised approach - Two pointer
+   let left1 = 0;
+   let left2 = 0;
+
+   while(left1<word1.length || left2<word2.length){
+
+        if(word1[left1] && word2[left2]){
+            result += word1[left1] + word2[left2];
+            left1++;
+            left2++;
         }
-        if(word2[i]){
-           result+= word2[i];
+        else if(word1[left1]){
+            result += word1[left1];
+            left1++;
+
         }
+        else{
 
-    }
+            result += word2[left2];
+            left2++;
 
-    return result;
+        }
+   }
+
+   return result;
+
     
 };
