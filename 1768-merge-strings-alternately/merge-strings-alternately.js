8badc53ee5b0e2c8dc173d5820e56130b7f83401
VijaySingh@8650 <rathorevijay995@gmail.com>
2025-01-04 14:03:00 +0530

    merge-string-alternatively-solved

---
+++ 1768-merge-strings-alternately/merge-strings-alternately.js
@@ -0,0 +1,49 @@
+
+
+var mergeAlternately = function(word1, word2) {
+
+    let result = "";
+    
+    // brute force
+    // for(let i=0; i< (word1.length + word2.length); i++){ // TC - O(n+m) SC - (1)
+
+    //     if(word1[i]){
+    //         result+= word1[i];
+    //     }
+    //     if(word2[i]){
+    //        result+= word2[i];
+    //     }
+
+    // }
+
+    // return result;
+   
+
+   //Optimised approach - Two pointer
+   let left1 = 0;
+   let left2 = 0;
+
+   while(left1<word1.length || left2<word2.length){ // TC - O(n>m ? n : m)  SC - O(1)
+
+        if(word1[left1] && word2[left2]){
+            result += word1[left1] + word2[left2];
+            left1++;
+            left2++;
+        }
+        else if(word1[left1]){
+            result += word1[left1];
+            left1++;
+
+        }
+        else{
+
+            result += word2[left2];
+            left2++;
+
+        }
+   }
+
+   return result;
+
+    
+};
